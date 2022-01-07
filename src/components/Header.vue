fca85ec8ed0eefc5cc30544538493fda34347c3b
marketingmatch <92303850+marketingmatch@users.noreply.github.com>
2022-01-07 10:20:46 +0100

    Add plugins

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row">
+    <div class="col logo-wrap">
+      <img
+        class="img-fluid logo"
+        src="../assets/img/logo.png"
+        alt="nasa logo"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import "../styles/style.scss";
+</style>
---
+++ src/components/Images.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="row"
+    id="lightgallery"
+    style="margin-top: 15px; justify-content: center"
+  >
+    <div
+      class="col-12 col-md-6 col-lg-3 card-nasa"
+      v-for="image in items"
+      :key="image.id"
+    >
+      <div class="card" :data-src="image.img_src" style="width: 18rem">
+        <a
+          class="item_gallery"
+          :href="image.img_src"
+          style="text-decoration: none; color: #000"
+        >
+          <img class="card-img-top" :src="image.img_src" alt="Card image cap" />
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">{{ image.id }}</h5>
+          <p class="card-text">
+            {{ image.earth_date }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["items"],
+};
+</script>
